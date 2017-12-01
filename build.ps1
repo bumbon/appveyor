ee24e030cfef1146d0874e01507cb54d9dffc098
SOFTSERVE\ishyy <ishyy@softserveinc.com>
2017-12-01 18:44:21 +0200

    try

--- build.ps1
+++ build.ps1
@@ -42,8 +42,8 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test .\AgileCraft.Api.Tests -c Release }
-exec { & dotnet test .\AgileCraft.DataAccess.Tests -c Release }
-exec { & dotnet test .\AgileCraft.Services.Tests -c Release }
+#exec { & dotnet test .\AgileCraft.Api.Tests -c Release }
+#exec { & dotnet test .\AgileCraft.DataAccess.Tests -c Release }
+#exec { & dotnet test .\AgileCraft.Services.Tests -c Release }
 
-exec { & dotnet pack .\AgileCraft.Api -c Release -o .\artifacts --version-suffix=$revision }
+#exec { & dotnet pack .\AgileCraft.Api -c Release -o .\artifacts --version-suffix=$revision }
